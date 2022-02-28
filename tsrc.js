a3115e16e942ad60bab2ac8f78fd60341c45728f
Devill Don <53294521+devillD@users.noreply.github.com>
2022-02-28 20:06:21 +0600

    Update tsrc.js

--- tsrc.js
+++ tsrc.js
@@ -10,7 +10,8 @@ async function get_torrent() {
   if(query !== ''){
     const api = "https://dev.abirxo.cf/" + siteName + "/" + query;
     
-    const html = await axios.get(api);
+    const html = await axios.get(api, {
+    headers: { 'User-Agent':'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36' }});
     const torrents = await html.data;
     console.log(torrents);
   
